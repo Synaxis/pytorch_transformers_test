a83f01c9c7cbadc3ac5346485cc639cf783d9b50
Synaxis <01xtor@gmail.com>
2024-03-30 16:39:26 -0300

    CPU only for now

--- llmtest.py
+++ llmtest.py
@@ -1,6 +1,8 @@
 # Import necessary libraries
 from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer
 from torch.utils.data import Dataset, DataLoader
+import os
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 import torch
 
 class TextDataset(Dataset):
@@ -29,10 +31,10 @@ model = GPT2LMHeadModel.from_pretrained('distilgpt2', config=configuration)
 
 # Load the dataset
 dataset = TextDataset(tokenizer, "dataset.txt")
-data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
+data_loader = DataLoader(dataset, batch_size=1, shuffle=True)  # set to 1 due to slow pc
 
 # Training settings
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 model.to(device)
 model.train()
 optimizer = torch.optim.Adam(model.parameters(), lr=5e-5)
@@ -52,4 +54,3 @@ for epoch in range(num_epochs):
 # Save the trained model
 model.save_pretrained('./my_model_directory/')
 tokenizer.save_pretrained('./my_model_directory/')
-
